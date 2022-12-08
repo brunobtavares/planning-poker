6577e8359e82cba5787d258b96a00ebf94538fbf
Bruno Tavares <TR039422@MinhaTI.com.br>
2022-12-07 23:27:33 -0300

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBQujecS6EtrN0HG8TCyW9DurdU5jgFKsk",
+    authDomain: "planning-poker-bbt.firebaseapp.com",
+    databaseURL: "https://planning-poker-bbt-default-rtdb.firebaseio.com",
+    projectId: "planning-poker-bbt",
+    storageBucket: "planning-poker-bbt.appspot.com",
+    messagingSenderId: "851619405106",
+    appId: "1:851619405106:web:57c50f725eb9bf7ba64b2b",
+    measurementId: "G-RW1DGDDRN4"
+  }
 };
