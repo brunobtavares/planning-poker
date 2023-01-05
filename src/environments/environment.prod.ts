8d00028dd5a7afee52011138188a419016f4722a
Bruno Tavares <TR039422@MinhaTI.com.br>
2023-01-05 17:57:02 -0300

    Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,6 @@ export const environment = {
     messagingSenderId: "851619405106",
     appId: "1:851619405106:web:57c50f725eb9bf7ba64b2b",
     measurementId: "G-RW1DGDDRN4"
-  }
+  },
+  encryptKey: "e47abaca-6f5f-47e4-bb1f-3157df5caa17"
 };
